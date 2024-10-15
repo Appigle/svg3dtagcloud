279116db57785bbf2b902ec79fb3cdd8cbde7db9
Chenley <chenley@qq.com>
2024-10-15 16:37:46 -0400

    build: add cp.sh

---
+++ scripts/cp.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+echo "-- Starting"
+cd ./scripts
+echo "-- Current directory"
+pwd
+echo "-- This script will copy the files from the current directory to the specified directory"
+cp ../dist/SVG3DTagCloud.umd.cjs ../dist/SVG3DTagCloud.global.js
+echo "-- Copy done!"
+echo "-- Back to parent directory"
+cd ..
+ls -lh ./dist
